6e2f63ef2d10452bdfc2e296e9452a6ee8696296
G Codes <graciela.mejia1@yahoo.com>
2022-08-21 12:14:51 -0700

    final ui ix improvments/font icons/customized styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,7 +65,108 @@ var auditTask = function (taskEl) {
   }
 };
 
+//enable draggable/sortable feature on list-group elements
+$(".card .list-group").sortable({
+  //enable dragging across lists
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  over: function (event) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function (event) {
+    $(event.target).removeClass("dropover-active");
+  },
+  update: function () {
+    var tempArr = [];
+
+    //loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function () {
+        //add task data to the temp array as an object
+        tempArr.push({
+          text: $(this).find("p").text().trim(),
+          date: $(this).find("span").text().trim(),
+        });
+      });
+
+    //trim down list's ID to match object property
+    var arrName = $(this).attr("id").replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+});
+
+//trash icon can be dropped onto
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    $("bottom-trash").removeClass("bottom-trash-active");
+  },
+  over: function (event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out: function (event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
+});
+
+//convert text field into jquery date picker
+$("#modalDueDate").datepicker({
+  minDate: 1,
+});
+
+// modal was triggered
+$("#task-form-modal").on("show.bs.modal", function () {
+  // clear values
+  $("#modalTaskDescription, #modalDueDate").val("");
+});
+
+// modal is fully visible
+$("#task-form-modal").on("shown.bs.modal", function () {
+  // highlight textarea
+  $("#modalTaskDescription").trigger("focus");
+});
+
+// save button in modal was clicked
+$("#task-form-modal .btn-save").click(function () {
+  // get form values
+  var taskText = $("#modalTaskDescription").val();
+  var taskDate = $("#modalDueDate").val();
+
+  if (taskText && taskDate) {
+    createTask(taskText, taskDate, "toDo");
+
+    // close modal
+    $("#task-form-modal").modal("hide");
+
+    // save in tasks array
+    tasks.toDo.push({
+      text: taskText,
+      date: taskDate,
+    });
+
+    saveTasks();
+  }
+});
+
+//task text was clicked
 $(".list-group").on("click", "p", function () {
+  //get current text of p element
   var text = $(this).text().trim();
 
   //replace p element with a new textarea
@@ -152,107 +253,6 @@ $(".list-group").on("change", "input[type='text']", function () {
   auditTask($(taskSpan).closest(".list-group-item"));
 });
 
-// modal was triggered
-$("#task-form-modal").on("show.bs.modal", function () {
-  // clear values
-  $("#modalTaskDescription, #modalDueDate").val("");
-});
-
-// modal is fully visible
-$("#task-form-modal").on("shown.bs.modal", function () {
-  // highlight textarea
-  $("#modalTaskDescription").trigger("focus");
-});
-
-// save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
-  // get form values
-  var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val();
-
-  if (taskText && taskDate) {
-    createTask(taskText, taskDate, "toDo");
-
-    // close modal
-    $("#task-form-modal").modal("hide");
-
-    // save in tasks array
-    tasks.toDo.push({
-      text: taskText,
-      date: taskDate,
-    });
-
-    saveTasks();
-  }
-});
-
-//enable draggable/sortable feature on list-group elements
-$(".card .list-group").sortable({
-  //enable dragging across lists
-  connectWith: $(".card .list-group"),
-  scroll: false,
-  tolerance: "pointer",
-  helper: "clone",
-  activate: function (event) {
-    console.log("activate", this);
-  },
-  deactivate: function (event) {
-    console.log("deactivate", this);
-  },
-  over: function (event) {
-    console.log("over", event.target);
-  },
-  out: function (event) {
-    console.log("out", event.target);
-  },
-  update: function (event) {
-    var tempArr = [];
-
-    //loop over current set of children in sortable list
-    $(this)
-      .children()
-      .each(function () {
-        var text = $(this).find("p").text().trim();
-
-        var date = $(this).find("span").text().trim();
-
-        //add task data to the temp array as an object
-        tempArr.push({
-          text: text,
-          date: date,
-        });
-      });
-
-    //trim down list's ID to match object property
-    var arrName = $(this).attr("id").replace("list-", "");
-
-    //update array on tasks object and save
-    tasks[arrName] = tempArr;
-    saveTasks();
-
-    console.log(tempArr);
-  },
-});
-
-$("#trash").droppable({
-  accept: ".card .list-group-item",
-  tolerance: "touch",
-  drop: function (event, ui) {
-    console.log("drop");
-    ui.draggable.remove();
-  },
-  over: function (event, ui) {
-    console.log("over");
-  },
-  out: function (event, ui) {
-    console.log("out");
-  },
-});
-
-$("#modalDueDate").datepicker({
-  minDate: 1,
-});
-
 // remove all tasks
 $("#remove-tasks").on("click", function () {
   for (var key in tasks) {
@@ -264,3 +264,9 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
+    auditTask(el);
+  });
+}, 1000 * 60 * 30);
